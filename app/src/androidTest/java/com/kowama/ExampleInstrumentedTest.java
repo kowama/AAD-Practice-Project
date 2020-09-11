b7e3d6b37301748e1e3119bc0794324f4a082b5e
kowama <n.kowama@gmail.com>
2020-09-11 22:41:16 +0100

    some refactor

--- app/src/androidTest/java/com/kowama/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kowama/ExampleInstrumentedTest.java
@@ -1,3 +1,4 @@
+/*
 package com.kowama;
 
 import android.content.Context;
@@ -10,11 +11,13 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+*/
 /**
  * Instrumented submit_successfuly_back_shape, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+ *//*
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -23,4 +26,4 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.pluralsight", appContext.getPackageName());
     }
-}
+}*/
